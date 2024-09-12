73653a61bf017e1c65b7a20bfef6af0f98f2460a
Jackson Arango Loaiza <jacksonsan009@hotmail.com>
2024-09-12 17:45:07 -0500

    Add delete button for the register

--- Components/Models/Debit.cs
+++ Components/Models/Debit.cs
@@ -4,10 +4,12 @@ namespace Contabilidad_APP.Components.Models
 {
     public class Debit
     {
+        private static int _nextId = 1;
         private decimal _amount;
         private DateTime _date;
         private string _description;
 
+        public int Id { get; set; } = _nextId++;
         public decimal Amount { get => _amount; set => _amount = value; }
         public DateTime Date { get => _date; set => _date = value; }
         public string Description { get => _description; set => _description = value; }
